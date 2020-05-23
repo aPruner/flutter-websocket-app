238f13e77a1d6923ff6f9da31e07275d991d3310
aPruner <adampruner99@gmail.com>
2020-05-23 15:27:09 -0700

    implement dotenv for server hostname, implement basic websocket message passing

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,18 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:web_socket_channel/io.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() => runApp(WebsocketApp());
+Future main() async {
+  await DotEnv().load();
+  print(DotEnv().env['SERVER_HOSTNAME']);
+  runApp(WebsocketApp());
+}
 
 class WebsocketApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    connectToWebsocket();
     return MaterialApp(
       title: 'My Websocket Chat App',
       home: Scaffold(
@@ -18,10 +26,33 @@ class WebsocketApp extends StatelessWidget {
               Text('This is text widget 2'),
               Text('This is text widget 3'),
             ]
-          )
+          ),
         ),
       ),
     );
   }
 
+  void connectToWebsocket() async {
+    String serverHostname = getServerHostname();
+    IOWebSocketChannel channel = IOWebSocketChannel.connect('ws://$serverHostname:4000/ws/chat');
+  
+    print('flutter app connected to the server');
+
+    channel.stream.listen((message) {
+      print('message received from server!');
+      print(message);
+    });
+  }
+
+  String encodeMessage(message) {
+    return jsonEncode({
+      'data': {
+        'message': message
+      }
+    });
+  }
+
+  String getServerHostname() {
+    return DotEnv().env['SERVER_HOSTNAME'];
+  }
 }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,18 +13,10 @@ import 'package:websocket_chat/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(WebsocketApp());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    expect(find.text('This is text widget 1'), findsOneWidget);
+    expect(find.text('This is text widget 2'), findsOneWidget);
+    expect(find.text('This is text widget 3'), findsOneWidget);
   });
 }
